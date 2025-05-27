fe314eeb7f324d1f1237930744dd49ebab35a8b1
Cherish <cherish.anpro@gmail.com>
2025-05-27 20:20:00 +0700

    footer

--- public/script.js
+++ public/script.js
@@ -66,3 +66,21 @@ document.addEventListener("DOMContentLoaded", () => {
       console.error("Gagal mengambil data produk:", error);
     });
 });
+
+fetch('footer.html')
+  .then(res => res.text())
+  .then(html => {
+    document.getElementById('footer').innerHTML = html;
+  });
+
+document.addEventListener("DOMContentLoaded", () => {
+  const footer = document.getElementById("footer");
+  if (footer) {
+    fetch("footer.html")
+      .then(res => res.text())
+      .then(html => {
+        footer.innerHTML = html;
+      })
+      .catch(err => console.error("Gagal load footer:", err));
+  }
+});
